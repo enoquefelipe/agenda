daa4cade277debbbafad4f62522e69132b525682
Enoque Leal <enoquefelipe@gmail.com>
2016-10-26 11:20:56 -0200

    Removi um sysout

--- app/src/main/java/com/enoqueleal/agenda/FormularioActivity.java
+++ app/src/main/java/com/enoqueleal/agenda/FormularioActivity.java
@@ -9,7 +9,5 @@ public class FormularioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
-
-        System.out.println("\n Errou");
     }
 }
